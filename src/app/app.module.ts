4a80b101d71af0d2d68b5bb1ee39a2e8f3e2d31c
pablosay <19001434@galileo.edu>
2023-10-05 16:14:36 -0600

    Configuration, Login template done

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { ButtonModule } from 'primeng/button';
+import {InputTextModule } from 'primeng/inputtext';
+
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MenubarModule } from 'primeng/menubar';
+
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
+import { MenuModule } from 'primeng/menu';
+import { InputMaskModule } from 'primeng/inputmask';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableModule } from 'primeng/table';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    ConfigurationComponent,
+    WelcomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ButtonModule,
+    InputTextModule,
+    NoopAnimationsModule,
+    MenubarModule,
+    MenuModule,
+    InputMaskModule,
+    DropdownModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    TableModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
